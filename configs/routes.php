6070fbb36be7f87ed993997bd2a6d551e1c21961
Sergey Nelepa <nelepa1995@mail.ru>
2016-02-27 00:11:24 +0300

    fixed pagination for main output news

--- configs/routes.php
+++ configs/routes.php
@@ -3,7 +3,8 @@
 return array(
     "news/category/([0-9]+)" => "news/category/$1",
     "news/view/([0-9]+)" => "news/view/$1",
-    "news/page-([0-9+])" => "news/list/$1",
+    "news/page-([0-9]+)" => "news/list/$1",
     "home" => "home/test",
+    "page-([0-9]+)" => "news/list/$1",
     "" => "news/list"
 );
--- views/news/newslist.php
+++ views/news/newslist.php
@@ -47,11 +47,11 @@
         <?php if($numberPages != 0): ?>
       <div class="container">
         <ul class="pagination">
-          <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <li class="<?php if($page==1){ echo "disabled";} else {echo "waves-effect";} ?>"><a href="<?php if($page != 1 ) echo '/page-'.($page-1) ?>"><i class="material-icons">chevron_left</i></a></li>
             <?php $thisPage = 0; while($thisPage++<$numberPages): ?>
           <li class="<?php if($thisPage == $page){echo "active";} else{ echo "waves-effect"; }?>"><a href="<?php echo '/page-'.$thisPage ?>"><?=$thisPage?></a></li>
                 <?php endwhile ?>
-          <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+          <li class="<?php if($page==$numberPages){ echo "disabled";} else {echo "waves-effect";} ?>"><a href="<?php if($page != $numberPages ) echo '/page-'.($page+1) ?>"><i class="material-icons">chevron_right</i></a></li>
         </ul>
       </div>
         <?php endif ?>
