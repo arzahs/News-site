47d3e8a0eba2f3694a30467bce78cbdaaa89443d
Sergey Nelepa <nelepa1995@mail.ru>
2016-02-27 20:34:00 +0300

    fixed work pagination with category

--- views/pagination.php
+++ views/pagination.php
@@ -1,9 +1,26 @@
 <div class="container">
         <ul class="pagination">
-          <li class="<?php if($page==1){ echo "disabled";} else {echo "waves-effect";} ?>"><a href="<?php if($page != 1 ) echo '/page-'.($page-1) ?>"><i class="material-icons">chevron_left</i></a></li>
+          <li class="<?php if($page==1){ echo "disabled";} else {echo "waves-effect";} ?>"><a href="<?php if($page != 1 ){
+                  if($activeCategory!=0){
+                      echo 'http://'.$_SERVER["HTTP_HOST"]."/news/category/".$activeCategory.'/page-'.($page-1);
+                  }else{
+                      echo '/page-'.($page-1);
+                  }
+                }?>"><i class="material-icons">chevron_left</i></a></li>
             <?php $thisPage = 0; while($thisPage++<$numberPages): ?>
-    <li class="<?php if($thisPage == $page){echo "active";} else{ echo "waves-effect"; }?>"><a href="<?php echo '/page-'.$thisPage ?>"><?=$thisPage?></a></li>
+    <li class="<?php if($thisPage == $page){echo "active";} else{ echo "waves-effect"; }?>"><a href="<?php
+        if($activeCategory!=0){
+            echo 'http://'.$_SERVER["HTTP_HOST"]."/news/category/".$activeCategory.'/page-'.$thisPage;
+        }else{
+            echo '/page-'.$thisPage;
+        } ?>"><?=$thisPage?></a></li>
 <?php endwhile ?>
-<li class="<?php if($page==$numberPages){ echo "disabled";} else {echo "waves-effect";} ?>"><a href="<?php if($page != $numberPages ) echo '/page-'.($page+1) ?>"><i class="material-icons">chevron_right</i></a></li>
+<li class="<?php if($page==$numberPages){ echo "disabled";} else {echo "waves-effect";} ?>"><a href="<?php if($page != $numberPages ){
+        if($activeCategory!=0){
+            echo 'http://'.$_SERVER["HTTP_HOST"]."/news/category/".$activeCategory.'/page-'.($page+1);
+        }else{
+            echo '/page-'.($page+1);
+        }
+    } ?>"><i class="material-icons">chevron_right</i></a></li>
 </ul>
 </div>
